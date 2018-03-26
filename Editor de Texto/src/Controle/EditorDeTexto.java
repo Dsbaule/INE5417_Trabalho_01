166a0b915d948175e64204cb758756cb2ad4e777
Daniel <Daniel@DESKTOP-RCDOQNE>
2018-03-26 17:27:32 -0300

    Iteração 1 - Versão 0.3
    - Leitura e escrita de arquivo txt implementados
    - Criação do controle

--- Editor de Texto/src/Controle/EditorDeTexto.java
+++ Editor de Texto/src/Controle/EditorDeTexto.java
@@ -7,12 +7,37 @@
 
 package Controle;
 
+import Dados.Documento;        
+import Tela.Tela_Principal;
+import javax.swing.JFrame;
+
 public class EditorDeTexto {
+    
+    enum codeState {
+            INICIO,
+            ABRIR,
+            EDIÇÃO,
+            SALVAR,
+            SAIR
+    }
     
     public static void main(String[] args) {
         
+        codeState estado = codeState.INICIO;
         
+        
+        while (estado != codeState.SAIR) {
+            switch (estado) {
+                case INICIO:
+                    initTela();
+                    break;
+            }
+        }
         
     }
     
+    private static void initTela() {
+        Tela_Principal tela = new Tela_Principal();
+        tela.setVisible(true);
+    }
 }
