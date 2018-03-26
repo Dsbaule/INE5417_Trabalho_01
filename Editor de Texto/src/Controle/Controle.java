134004ba385f844a1592366cbb3d6718cd9e3fbf
MirianFS <mirianfloripa16@gmail.com>
2018-03-26 17:51:03 -0300

    foi criar duas historia de usuario (editar texto e salvar). obs: estao caminho pre definido. obs2: não sei se ele respeita a quebra de linha. obs3: fiz uma classe chamada arquivo no pacote modelo.

---
+++ Editor de Texto/src/Controle/Controle.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controle;
+
+import Modelo.Arquivo;
+import Tela.Tela_Principal;
+import java.nio.file.Files;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Mirian
+ */
+public class Controle {
+    
+    
+    Tela_Principal tela;
+    Arquivo arquivo;
+    
+    
+    public Controle(){
+             tela = new Tela_Principal();
+             arquivo = new Arquivo();
+    }
+   
+    
+    public void abrirJanela (){
+        // Serve para abrir a Tela_principal
+        // abre o sistema
+        tela.setVisible(true); 
+    }
+    
+    public void GravarArquivo (){
+        // esse metodo serve tanto para Salvar quanto para Salvar como... -- Atualmente serve somente para Salvar
+        // atualmente o caminha esta para o meu pc
+        // essa duas primeiras linhas é só para teste
+        arquivo.setNome("teste");
+        arquivo.setCaminho("C:/Users/Mirian/Desktop/");
+        
+        Path caminho = Paths.get(arquivo.getCaminho()+arquivo.getNome() +".txt");
+        String texto = arquivo.getTexto();
+        byte[] textoEmBytes = texto.getBytes();
+        try{
+            Files.write(caminho, textoEmBytes);
+        }catch (Exception erro){
+            System.err.println(erro);
+        }
+        
+    }
+    
+     public void salvar (String t){
+        //set texto no objeto "arquivo" 
+         arquivo.setTexto(t);
+         // executa o metodo que grava o arquivo
+         GravarArquivo();
+         
+         
+         //
+         
+    }
+     
+    public void Novo (String t){
+        
+//        arquivo.setTexto(t);
+//        
+//        if("".equals(arquivo.getTexto().trim()) && "".equals(arquivo.getCaminho().trim())){
+//        //Arquivo vazio
+//            System.out.println("vazio:"+ "\n" +arquivo.getTexto());
+//        }else{
+//            int r = JOptionPane.showConfirmDialog(null,"Deseja salvar alterações?", "Salvar", JOptionPane.YES_NO_OPTION);
+//            
+//            if(r == 1){
+//                // r = não, então esvazia texto
+//                arquivo.setTexto("");
+//            }
+//            // Obs: resposta == sim, r = 0
+//        }
+//        
+        
+        /* ----------------------------------------------- */
+//        if("".equals(t.trim())){
+//            System.out.println("vazio:"+ "\n" +tela.getTexto());
+//        }else{
+//            int r = JOptionPane.showConfirmDialog(null,"Deseja salvar alterações?", "Salvar", JOptionPane.YES_NO_OPTION);
+//            
+//            if(r == 1){
+//                // r = não, então esvazia texto
+//                tela.setTextoVazio();
+//            }
+//            // Obs: resposta == sim, r = 0
+//        }
+        //new Tela_Principal().setVisible(true);
+        
+    }
+     
+}
