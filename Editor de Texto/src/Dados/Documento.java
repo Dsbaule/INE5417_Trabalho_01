5d1a2714032675c091de089cdcfa156fb8d5b4bc
Daniel <Daniel@DESKTOP-RCDOQNE>
2018-03-27 09:01:14 -0300

    Iteração 01 - Versão 0.7
    - Remoção dos arquivos não utilizados
    - Adição de comeentários

--- Editor de Texto/src/Dados/Documento.java
+++ Editor de Texto/src/Dados/Documento.java
@@ -4,35 +4,42 @@
  * Disciplina:  INE5417 - Engenharia de Software
  * Projeto:     Edito de Texto
 -------------------------------------------------------------------------------- */
+
+//! Declaração do pacote
 package Dados;
 
+//! Importações necessárias
 import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+//! Classe para implementação de um documento
 public class Documento {
-
-    private File arquivo;
-    private String texto;
-
+    
+    private File arquivo;   // Caminho para o arquivo representado
+    private String texto;   // Texto do documento
+    
+    //! Construtor que recebe o caminho para o arquivo (Como File)
     public Documento(File arquivo) {
         this.arquivo = arquivo;
         lerArquivo();
     }
-
+    
+    //! Construtor que recebe o caminho para o arquivo (Como String)
     public Documento(String arquivo) {
         this.arquivo = new File(arquivo);
         lerArquivo();
     }
     
+    //! Construtor que recebe a pasta e o nome do arquivo (Geralmente para criação de novo arquivo)
     public Documento(File pasta, String nome) {
-        if(!nome.endsWith(".txt"))
-            nome = nome.concat(".txt");
-        System.out.println(nome);
+        if(!nome.endsWith(".txt"))      // Garante que o arquivo termine em .txt
+            nome = nome.concat(".txt"); 
         this.arquivo = new File(pasta, nome);
         lerArquivo();
     }
-
+    
+    //! Le o conteúdo do arquivo
     private void lerArquivo() {
         FileInputStream fis = null;
         try {
@@ -44,27 +51,29 @@ public class Documento {
         } catch (FileNotFoundException ex) {
             System.out.println("Arquivo não encontrado, criando...");
             this.texto = "";
-            //Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(Documento.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    
+    //! Salva o conteúdo no arquivo
     public void salvarArquivo() {
         try {
             FileWriter fstream = new FileWriter(arquivo);
             BufferedWriter out = new BufferedWriter(fstream);
             out.write(texto);
             out.close();
-        } catch (Exception e) {//Catch exception if any
+        } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         }
     }
     
+    //! Retorna o texto do documento
     public String getTexto() {
         return this.texto;
     }
     
+    //! Altera o texto do documento
     public void setTexto(String texto) {
         this.texto = texto;
     }
